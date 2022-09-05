42c1345a176fd2ca42e032cad7dce4d64fe84519
iluvgirlswithglasses <luunamdat204@gmail.com>
2022-09-05 13:55:02 +0700

    file & directory control. html doesn't support directory downloading heh?

--- README.md
+++ README.md
@@ -10,3 +10,18 @@ When you need to quickly copy a file from a computer to another, what would you 
 And that's why I created this abomination:
 
 ![2022-09-05_13-28](https://user-images.githubusercontent.com/58514512/188375626-b84737d5-f06c-4879-820b-d30e22d221a0.png)
+
+## How to use
+
+Create this directory if not exists:
+
+```
+./portal/static/f/
+```
+
+Let's call that directory as `ROOT`.
+
+In `ROOT`, create some more directories as channels. Put files to those channels to get them shared. Sounds like Discord, heh?
+
+To run server, simply follow `manage.py`'s instruction. For example: `python manage.py runserver 0.0.0.0:4002`
+
--- portal/views.py
+++ portal/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
-import os
+from os import listdir
+from os.path import isdir, isfile, join
+
+STATIC_URL = "portal/static/f/"
 
 def get(request):
-    # this portal works like a discord server
-    # you put your directories - which work as channels - here
-    STATIC_URL = os.getcwd() + "/portal/static/f/"
-    lst = {}
-    for channel in sorted(os.listdir(STATIC_URL)):
-        if len(os.listdir(STATIC_URL + channel)) > 0:
-            # and then you put your content into the channel
-            lst[channel] = sorted(os.listdir(STATIC_URL + channel))
-    # then everything is done
-    return render(request, "homepage.html", {
-        "lst": lst,
-    })
+	# this portal works like a discord server
+	# you put your directories - which work as channels - here
+	lst = {}
+	for channel in sorted(listdir(STATIC_URL)):
+		f = STATIC_URL + channel
+		if isdir(f) and len(listdir(f)) > 0:
+			# and then you put your content into the channel
+			lst[channel] = sorted([x for x in listdir(f) if isfile(join(f, x))])
+	# then everything is done
+	return render(request, "homepage.html", {
+		"lst": lst,
+	})
